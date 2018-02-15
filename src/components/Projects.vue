a30412a6fedb1bcc3bada2caf7dfba5db8f709ef
João Eudes Lima <joaoeudes7@gmail.com>
2018-02-14 22:03:36 -0300

    Update Projects

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,67 +1,92 @@
 <template>
-	<div>
-		<v-card>
-			<v-subheader>Repositories</v-subheader>
-			<div v-if="projects.length > 0">
-				<v-list two-line>
-					<div v-for="project in projects" v-bind:key="project.id">
-						<v-list-tile avatar>
-							<v-list-tile-avatar>
-								<img src="https://avatars1.githubusercontent.com/u/16367752?v=4">
-							</v-list-tile-avatar>
-							<v-list-tile-content>
-								<v-list-tile-title v-html="project.name"></v-list-tile-title>
-								<v-list-tile-sub-title v-html="project.description"></v-list-tile-sub-title>
-							</v-list-tile-content>
-						</v-list-tile>
-					</div>
-				</v-list>
+  <div>
+    <v-subheader>Repositories</v-subheader>
+    <v-card>
+      <div v-if="projects.length > 0">
+        <v-list two-line>
+          <div v-for="(project, index) in projects" v-bind:key="project.id">
 
-			</div>
-			<div v-else>
-				<md-progress-spinner class="md-accent" :md-stroke="3" md-mode="indeterminate"></md-progress-spinner>
-			</div>
-		</v-card>
-	</div>
+            <v-list-tile avatar>
+              <v-list-tile-avatar>
+                <img src="https://avatars1.githubusercontent.com/u/16367752?v=4">
+              </v-list-tile-avatar>
+              <v-list-tile-content>
+                <v-list-tile-title><a :href="project.html_url">{{project.name}}</a></v-list-tile-title>
+                <v-list-tile-sub-title v-html="project.description"></v-list-tile-sub-title>
+              </v-list-tile-content>
+              <v-list-tile-action>
+                <v-list-tile-action-text v-html="formatDate(project.created_at)"></v-list-tile-action-text>
+              </v-list-tile-action>
+            </v-list-tile>
+            <v-divider v-if="index+1 < projects.length" :key="`divider-${index}`"></v-divider>
+
+          </div>
+        </v-list>
+
+      </div>
+      <div v-else>
+        <v-progress-circular indeterminate :size="100" color="primary"></v-progress-circular>
+      </div>
+    </v-card>
+  </div>
 </template>
 
 <script>
-	import axios from "axios";
+  import axios from 'axios';
+  // import moment from 'moment';
 
-	export default {
-		name: "projects",
-		data() {
-			return {
-				projects: []
-			};
-		},
+  export default {
+    name: 'projects',
+    data() {
+      return {
+        projects: []
+      };
+    },
 
-		created() {
-			this.getProjects();
-		},
+    created() {
+      this.getProjects();
+    },
 
-		methods: {
-			getProjects() {
-				axios
-					.get(`https://api.github.com/users/joaoeudes7/repos`)
-					.then(response => {
-						// JSON responses are automatically parsed.
-						this.projects = response.data;
-					})
-					.catch(e => {
-						this.errors.push(e);
-					});
-			}
-		}
-	};
+    methods: {
+
+      getProjects() {
+        axios
+          .get(`https://api.github.com/users/joaoeudes7/repos`)
+          .then(response => {
+            this.projects = response.data;
+          })
+          .catch(e => {
+            this.errors.push(e);
+          });
+      },
+
+      formatDate(date) {
+        return new Date(date).toLocaleDateString();
+      }
+    }
+  };
 </script>
 
 <style scoped>
-	.md-list {
-		width: 620px;
-		max-width: 100%;
-		display: inline-block;
-		vertical-align: top;
-		border: 1px solid rgba(#000, .12);
-	}
+  .md-list {
+    width: 620px;
+    max-width: 100%;
+    display: inline-block;
+    vertical-align: top;
+    border: 1px solid rgba(#000, .12);
+  }
+
+  a:link,
+  a:visited {
+    text-decoration: none
+  }
+
+  a:hover {
+    text-decoration: none;
+    color: #0059b1
+  }
+
+  a:active {
+    text-decoration: none
+  }
 </style>
