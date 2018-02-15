caf54fa2418b30607311b1bf3b6c150d04dd97b3
João Eudes Lima <joaoeudes7@gmail.com>
2018-02-14 22:00:17 -0300

    Update Routers

--- src/router/index.js
+++ src/router/index.js
@@ -1,34 +1,29 @@
-import Vue from 'vue'
-import Router from 'vue-router'
+import Vue from 'vue';
+import Router from 'vue-router';
 
 // Components
-const Profile = () => import(/* webpackChunkName: "group-foo" */ '../components/Profile.vue')
-const Projects = () => import(/* webpackChunkName: "group-foo" */ '../components/Projects.vue')
+const Profile = () => import(/* webpackChunkName: "group-foo" */ '../components/Profile.vue');
+const Projects = () => import(/* webpackChunkName: "group-foo" */ '../components/Projects.vue');
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
     {
       path: '/',
       name: 'profile',
-      component: Profile
-		},
-		{
-      path: '/Profile',
+      component: Profile,
+    },
+    {
+      path: '/profile',
       name: 'Profile',
-      component: Profile
-		},
-		{
-      path: '/Projects',
+      component: Profile,
+    },
+    {
+      path: '/projects',
       name: 'Projects',
-      component: Projects
-		},
-		// {
-    //   path: '/Contact',
-    //   name: 'Project',
-    //   component: Contact
-		// },
+      component: Projects,
+    },
+  ],
+});
 
-  ]
-})
